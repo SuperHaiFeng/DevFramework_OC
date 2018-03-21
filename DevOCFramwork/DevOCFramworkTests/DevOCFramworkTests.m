bed7019add4c248955ee69d76ff1715bb040de36
志方 <zhangzhifang@xiniubox.com>
2018-03-21 17:21:28 +0800

    add Tests

--- DevOCFramwork/DevOCFramworkTests/DevOCFramworkTests.m
+++ DevOCFramwork/DevOCFramworkTests/DevOCFramworkTests.m
@@ -7,6 +7,29 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "ZFNetworkManager.h"
+#import "ZFNetworkManager+Extension.h"
+
+#define assertTrue(expr)            XCTAssertTrue((expr),@"")
+#define assertFalse(expr)           XCTAssertFalse((expr), @"")
+#define assertNil(a1)               XCTAssertNil((a1), @"")
+#define assertNotNil(a1)            XCTAssertNotNil((a1), @"")
+#define assertEqual(a1,a2)          XCTAssertEqual((a1), (a2), @"")
+#define assertEqualObjects(a1,a2)   XCTAssertEqualObjects((a1), (a2), @"")
+#define assertNotEqual(a1,a2)       XCTAssertNotEqual((a1), (a2),@"")
+#define assertNotEqualObjects(a1,a2)XCTAssertNotEqualObjects((a1), (a2),@"")
+#define assertAccuracy(a1,a2,acc)   XCTAssertEqualWithAccuracy((a1), (a2), (acc))
+
+#define WAIT                                                            \
+do {                                                                    \
+[self expectationForNotification:@"LCUnitTest" object:nil handler:nil]; \
+[self waitForExpectationsWithTimeout:60 handler:nil];                   \
+} while(0);
+
+#define NOTIFY                                                      \
+do {                                                                 \
+[[NSNotificationCenter defaultCenter] postNotificationName:@"LCUnitTest" object:nil]; \
+} while(0);
 
 @interface DevOCFramworkTests : XCTestCase
 
@@ -34,6 +57,14 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+-(void) testHomeWeiboReqeust {
+    [ZFNetworkManager.shared statusList:0 max_id:0 completion:^(NSString *list, BOOL isSuccess) {
+        assertNotNil(list);
+        NOTIFY
+    }];
+    WAIT
 }
 
 @end
